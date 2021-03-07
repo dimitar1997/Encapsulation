92bcd93e6da7e4acb6677935d5edf328c106df35
dimitar1997 <chakarov.1997@abv.bg>
2021-03-07 12:35:47 +0200

    Encapsulation

---
+++ src/ClassBox/Main.java
@@ -0,0 +1,27 @@
+package ClassBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double length = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+
+        Box box = null;
+        try {
+            box = new Box(length, width, height);
+        } catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        if (box != null){
+            System.out.printf("Surface Area - %.2f%n", box.calculateSurfaceArea());
+            System.out.printf("Lateral Surface Area - %.2f%n", box.calculateLateralSurfaceArea());
+            System.out.printf("Volume – %.2f%n", box.calculateVolume());
+        }
+
+    }
+}
